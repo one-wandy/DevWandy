f1b9edfcb149a5ffe6f013585f40b4a291d7d72d
Wandy Olivares <one.wandy@icloud.com>
2024-03-25 12:08:30 -0400

    Maps Basic

--- FyCas/FyCas/urls.py
+++ FyCas/FyCas/urls.py
@@ -4,6 +4,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('', include('Customer.urls')),
+     path('', include('Maps.urls')),
     path('admin/', admin.site.urls),
 ]
 if  settings.DEBUG:
--- FyCas/Maps/views.py
+++ FyCas/Maps/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views.generic import *
+from . import models
+from django.http import HttpResponseRedirect
+from django.urls import reverse_lazy, reverse
+from django.shortcuts import redirect
+import os
+from datetime import datetime
 
-# Create your views here.
+
+
+    
+class Maps(TemplateView):
+      template_name = "maps/location.html"
