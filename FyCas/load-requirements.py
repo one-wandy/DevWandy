dedf7e4753ecbd3fe14d2ca256f460105c15380b
Wandy Olivares <one.wandy@icloud.com>
2024-04-22 14:29:01 -0400

    sa

--- FyCas/load-requirements.py
+++ FyCas/load-requirements.py
@@ -1,5 +1,8 @@
 import subprocess
+
 # Ruta al archivo requirements.txt
 path_requirements = "Utils/requirements.txt"
-# Ejecutar el comando pip install
-subprocess.run(["pip", "freeze", ">", path_requirements ])
+
+# Ejecutar el comando pip freeze y redirigir la salida al archivo
+with open(path_requirements, "w") as f:
+    subprocess.run(["pip", "freeze"], stdout=f)
