781bdc4a80b5dd13c6b54536e3cd669a2c82e7b6
Wandy Olivares <one.wandy@icloud.com>
2024-05-02 01:23:02 -0400

    Mejoras

--- FyCas/FyCas/settings.py
+++ FyCas/FyCas/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -135,3 +135,5 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 GOOGLE_MAPS_API_KEY  = 'AIzaSyAfnJbgtHm5EZLkozG5Dzo_cgkeJndxNjA'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
