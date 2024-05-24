bf915c93797fa2346599a50b84734404e37b3b63
Wandy Olivares <one.wandy@icloud.com>
2024-05-24 13:50:43 -0400

    Sasm

--- FyCas/Customer/views.py
+++ FyCas/Customer/views.py
@@ -388,6 +388,7 @@ class NoApproved(TemplateView, Options):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['setting'] = self.Setting()
+        context['img'] = self.ImgApp(5)
         return context
     
     
