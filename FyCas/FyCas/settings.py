6ed7caf08eb8b87bcb8fcce4ba759800ccfcd4bc
Wandy Olivares <untal.wandy@gmail.com>
2025-01-10 13:29:23 -0400

    Algunas eliminaciones

--- FyCas/FyCas/settings.py
+++ FyCas/FyCas/settings.py
@@ -108,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-US'
+LANGUAGE_CODE = 'es-mx'
 TIME_ZONE = 'America/Santo_Domingo'
 USE_I18N = True
 USE_L10N = True
