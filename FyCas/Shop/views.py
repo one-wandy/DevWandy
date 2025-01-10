a1e86be1f08364afc32a86f5fc13747ab2b45d52
Wandy Olivares <untal.wandy@gmail.com>
2025-01-10 15:21:26 -0400

    Agregar plantilla de inventario y ajustar rutas de plantilla

--- FyCas/Shop/views.py
+++ FyCas/Shop/views.py
@@ -14,7 +14,7 @@ class Shop(TemplateView, Options):
 
 
 class Inventario(TemplateView, Options):
-    template_name = "shop/inventario.html"
+    template_name = "inventario/inventario.html"
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
